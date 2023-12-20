eb2e4e256bdb8d18c9c2147d43860654c2ff051d
kishoraditya <kishoradityasc@gmail.com>
2023-12-20 16:55:52 +0530

    my first comit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-+o%9bad+^)p+6om17*j7b2v_*j30cwcws9pa7q!k=6c938n37l"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
